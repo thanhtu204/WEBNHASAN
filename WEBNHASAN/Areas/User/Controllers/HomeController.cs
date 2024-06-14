9033b4b55edf11e3272d9eee6ba70f28a390aca7
ADMIN <ADMIN@PC234>
2024-06-14 11:25:31 +0700

    Add project files.

---
+++ WEBNHASAN/Areas/User/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using WEBNHASAN.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Http;
+using System.IO;
+namespace WEBNHASAN.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _db;
+
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext db)
+        {
+            _logger = logger;
+            _db = db;
+
+        }
+
+        public IActionResult Index(int? page )
+        {
+            var pageIndex = (int)(page != null ? page : 1);
+            var pageSize = 6;
+            var productList = _db.Products.Include(x => x.Category).ToList();
+            var pageSum = productList.Count() / pageSize + (productList.Count() % pageSize > 0 ? 1 : 0);
+            ViewBag.PageSum = pageSum;
+            ViewBag.PagIndex = pageIndex;
+            return View(productList.Skip((pageIndex - 1) * pageSize).Take(pageSize).ToList());
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ WEBNHASAN/wwwroot/js/site.js
@@ -0,0 +1,39 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+$(document).ready(function () {
+    showQuantiyCart();
+});
+
+
+$(".addtocart").click(function (evt) {
+    evt.preventDefault();
+    let id = $(this).attr("data-productId");
+
+    $.ajax({
+        url: "/User/Cart/AddToCartAPI",
+        data: { "productId": id },
+        success: function (data) {
+            //thong bao ket qua
+            Swal.fire({
+                title: "Product added to cart",
+                text: "You clicked the button!",
+                icon: "success"
+            });
+            //hien thi so luong san pham trong gio trong _FrontEnd.cshtml
+            showQuantiyCart();
+        }
+    });
+})
+
+
+let showQuantiyCart = () => {
+    $.ajax({
+        url: "/User/Cart/GetQuantityOfCart",
+        success: function (data) {
+            //hien thi so luong san pham trong gio trong _FrontEnd.cshtml
+            $(".showcart").text(data.qty);
+        }
+    });
+}
